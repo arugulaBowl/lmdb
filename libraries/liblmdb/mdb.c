58d12fb3abd8d50b7a81d435c6409bb1bc1af9f5
Arugula Bowl <arugula.bowl@gmail.com>
2018-07-01 20:45:28 +0000

    Attempt to fix windows compilation

--- libraries/liblmdb/mdb.c
+++ libraries/liblmdb/mdb.c
@@ -43,6 +43,8 @@
 #include <windows.h>
 #include <wchar.h>				/* get wcscpy() */
 
+typedef LONG NTSTATUS;
+
 /* We use native NT APIs to setup the memory map, so that we can
  * let the DB file grow incrementally instead of always preallocating
  * the full size. These APIs are defined in <wdm.h> and <ntifs.h>
